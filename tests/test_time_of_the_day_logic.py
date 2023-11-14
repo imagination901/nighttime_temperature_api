6060073d6dd18989725309c9698c7df729a14430
Avor Sky <1737000@gmail.com>
2023-11-14 12:21:28 -0500

    Add more tests

---
+++ tests/test_time_of_the_day_logic.py
@@ -0,0 +1,29 @@
+from models import SunriseSunsetData
+import datetime
+from temperature_calculator import get_temp
+
+
+morning_twilight_start= datetime.datetime(2023, 11, 14, 5, 13, 38) 
+sunrise=                datetime.datetime(2023, 11, 14, 5, 34, 17) 
+morning_twilight_end=   datetime.datetime(2023, 11, 14, 5, 54, 56) 
+noon=                   datetime.datetime(2023, 11, 14, 11, 40, 23) 
+evening_twilight_start= datetime.datetime(2023, 11, 14, 17, 25, 49) 
+sunset=                 datetime.datetime(2023, 11, 14, 17, 46, 28) 
+evening_twilight_end=   datetime.datetime(2023, 11, 14, 18, 7, 7)
+
+test_ssd = SunriseSunsetData(sunrise=sunrise,
+                             sunset=sunset,
+                             noon=noon,
+                             morning_twilight_start=morning_twilight_start,
+                             morning_twilight_end=morning_twilight_end,
+                             evening_twilight_start=evening_twilight_start,
+                             evening_twilight_end=evening_twilight_end)
+
+def test_night_get_temp():
+    night_now = datetime.datetime(2023, 11, 14, 1, 1, 1)
+    assert get_temp(now=night_now, sun_set_data=test_ssd) == 2700
+
+
+def test_day_get_temp():
+    night_now = datetime.datetime(2023, 11, 14, 11, 1, 1)
+    assert get_temp(now=night_now, sun_set_data=test_ssd) == 6000
